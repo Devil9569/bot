8cc80c7d0b8e165d95caaa00d68b27a38c1366db
Devil9569 <107525616+Devil9569@users.noreply.github.com>
2025-02-15 19:26:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -222,4 +222,4 @@ def index():
     return render_template_string(html_content)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host="127.0.0.1", port=5000)
